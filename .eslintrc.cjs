4b14518ff512b7e682641143787580003d7a6887
Zero的Mac <1203970284@qq.com>
2023-12-01 20:37:30 +0800

    feat: first commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,11 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
   plugins: ["@typescript-eslint/eslint-plugin"],
   extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
   root: true,
