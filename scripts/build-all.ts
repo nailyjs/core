115532fb0c8d33b5d87a50352cf3977c43daa912
Naily <zero@naily.cc>
2024-10-26 18:52:05 +0800

    feat: 📃auto upload docs

--- scripts/build-all.ts
+++ scripts/build-all.ts
@@ -1,11 +1,10 @@
-import { exec } from 'node:child_process'
-import { stderr, stdout } from 'node:process'
+import { execSync } from 'node:child_process'
 import { scripts } from '../package.json'
 
 for (const [name, _script] of Object.entries(scripts)) {
   if (!name.startsWith('build:')) continue
 
-  const child_process = exec(`pnpm run ${name}`)
-  child_process.stdout?.pipe(stdout)
-  child_process.stderr?.pipe(stderr)
+  execSync(`pnpm run ${name}`, {
+    stdio: 'inherit',
+  })
 }
