d179f4af7d513d876c94f65b01076b8aae50993e
Naily <zero@naily.cc>
2024-10-29 23:18:18 +0800

    feat: update deps

--- fixtures/backend/src/test.controller.ts
+++ fixtures/backend/src/test.controller.ts
@@ -1,17 +1,10 @@
-/* eslint-disable ts/consistent-type-imports */
-
 import { Get, RestController } from '@nailyjs/backend'
 import { Value } from '@nailyjs/config'
-import { Autowired } from '@nailyjs/ioc'
-import { JexlExecutor } from '@nailyjs/jexl'
 
 @RestController()
 export class TestController {
   @Value('1 + 1')
   private readonly hello: string
-
-  @Autowired()
-  private readonly jexl: JexlExecutor
 
   @Get()
   getString(): string {
