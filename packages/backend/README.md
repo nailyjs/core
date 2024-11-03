aa6c8777cc19ed414dac9ba0114f511e951368b8
Naily <zero@naily.cc>
2024-11-03 20:33:33 +0800

    feat: update readme

---
+++ packages/backend/README.md
@@ -0,0 +1,14 @@
+<div align="center">
+<br />
+
+<img src="https://universervice.oss-cn-guangzhou.aliyuncs.com/logo.png" width="150" />
+
+# Naily
+
+Naily is a IOC framework for TypeScript.
+
+</div>
+
+## @nailyjs/backend
+
+The backend package of naily, provides the http server backend features. It can support `node`/`deno`/`bun` and more environments using different adapters.
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,3 +1,25 @@
+/**
+ * # `@nailyjs/backend`
+ *
+ * The backend package of naily, provides the http server backend features. It can support `node`/`deno`/`bun` and more environments using different adapters.
+ *
+ * ## Note for users
+ *
+ * If you quickly start, you can see `@nailyjs/backend/node-adapter` for the node adapter.
+ *
+ * @module
+ * @example
+ * ```typescript
+ * import { BackendBootstrap } from '@nailyjs/backend'
+ *
+ * const app = new BackendBootstrap()
+ * app.setBackendAdapter(
+ *   // Your custom or internal adapter
+ * )
+ * app.run()
+ * ```
+ */
+
 export * from './backend-bootstrap'
 export * from './constant'
 export * from './contexts'
