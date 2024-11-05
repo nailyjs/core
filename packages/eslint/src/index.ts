af0ff57f315ac402cbb11811424b1077aaca0f2b
Naily <zero@naily.cc>
2024-11-06 01:59:18 +0800

    docs(changeset): feat(eslint): add comment tips

--- packages/eslint/src/index.ts
+++ packages/eslint/src/index.ts
@@ -15,6 +15,33 @@ export function naily(...args: Parameters<typeof antfu>): ReturnType<typeof antf
   }, ...args.slice(1))
 }
 
+/**
+ * Create a proxy to `naily.config.ts` for ESLint.
+ *
+ * @export
+ * @return {Promise<ReturnType<typeof antfu>>}
+ * @example
+ * In `eslint.config.js`:
+ *
+ * ```js
+ * import { nailyProxy } from '@nailyjs/eslint'
+ *
+ * export default await nailyProxy()
+ * ```
+ *
+ * Then you can define eslint config in `naily.config.ts`:
+ * ```ts
+ * import { defineConfig } from '@nailyjs/config'
+ *
+ * export default defineConfig({
+ *   naily: {
+ *     eslint: {
+ *       // Your eslint config here
+ *     }
+ *   }
+ * })
+ * ```
+ */
 export async function nailyProxy(): Promise<ReturnType<typeof antfu>> {
   return await new ESLintBootstrap()
     .use(ConfigPlugin())
--- packages/eslint/src/types.ts
+++ packages/eslint/src/types.ts
@@ -4,6 +4,18 @@ declare global {
   namespace Naily {
     namespace Configuration {
       interface NailyUserConfig {
+        /**
+         * Eslint config.
+         *
+         * If you want to define eslint config into this object, you must create `eslint.config.js` file in the root of your project,
+         * and add the following code:
+         * ```js
+         * import { nailyProxy } from '@nailyjs/eslint'
+         *
+         * export default await nailyProxy()
+         * ```
+         * It will proxy the config from `naily.config.ts` to this `naily.eslint` object.
+         */
         eslint?: Parameters<typeof antfu>[0] & {
           extraOptions?: Array<Parameters<typeof antfu>[1]>
         }
