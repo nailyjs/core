440f62818a5e4001fcaefa41445a65bc5491ac4d
Naily <zero@naily.cc>
2024-11-07 12:13:32 +0800

    docs(changeset): feat(cli): add export

---
+++ .changeset/cold-impalas-doubt.md
@@ -0,0 +1,5 @@
+---
+"@nailyjs/cli": patch
+---
+
+feat(cli): add export
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,36 +1,9 @@
-import { argv } from 'node:process'
-import { AbstractBootstrap } from '@nailyjs/ioc'
-import { Command, program } from 'commander'
-import { DevelopmentStarter } from './development.service'
-import { NewProjectCreator } from './new-project'
-import { LogoWriter } from './write-logo'
-
-export class CliBootstrap extends AbstractBootstrap {
-  private readonly _newProjectCreator: NewProjectCreator = new NewProjectCreator()
-  private readonly _developmentStarter = DevelopmentStarter.getInstance(this)
-
-  setupCommander(): Command {
-    program
-      .name('naily')
-      .version('0.0.1', '-v, --version', 'Output the current version')
-      .description('Naily CLI')
-
-    program.command('new')
-      .alias('n')
-      .description('Create a new naily project')
-      .action(this._newProjectCreator.setup.bind(this._newProjectCreator))
-
-    program.command('dev')
-      .alias('d')
-      .description('Start the development server')
-      .action(this._developmentStarter.setup.bind(this._developmentStarter))
-
-    return program
-  }
-
-  async run(): Promise<void> {
-    await this.getPluginRunner().runBeforeRun()
-    LogoWriter.getInstance(this).write(true)
-    this.setupCommander().parse(argv)
-  }
-}
+export * from './cli-bootstrap'
+export * from './development.service'
+export * from './entry-analyzer.service'
+export * from './new-project'
+export * from './package-file.service'
+export * from './production.service'
+export * from './tsup.service'
+export * from './types'
+export * from './write-logo'
