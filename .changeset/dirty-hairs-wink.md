6043fe638b7284b09bc12f4f118af4913f33a125
Naily <zero@naily.cc>
2024-11-10 14:59:45 +0800

    docs(changeset): fix(typeorm): remove typeorm/browser

---
+++ .changeset/dirty-hairs-wink.md
@@ -0,0 +1,5 @@
+---
+"@nailyjs/typeorm": patch
+---
+
+fix(typeorm): remove typeorm/browser
--- packages/typeorm/src/datasource.service.ts
+++ packages/typeorm/src/datasource.service.ts
@@ -1,7 +1,6 @@
 import { Value } from '@nailyjs/config'
 import { Autowired, Container, Injectable, Optional } from '@nailyjs/ioc'
-import { DataSource } from 'typeorm'
-import { DataSourceOptions } from 'typeorm/browser'
+import { DataSource, DataSourceOptions } from 'typeorm'
 import { CustomDataSource } from './custom.protocol'
 import { Entities } from './types'
 
