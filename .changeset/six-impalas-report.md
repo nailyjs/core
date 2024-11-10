a84817ed770b9a4b1085d2d708e1892cd8772694
Naily <zero@naily.cc>
2024-11-10 16:11:27 +0800

    docs(changeset): feat(typeorm): add global types

---
+++ .changeset/six-impalas-report.md
@@ -0,0 +1,5 @@
+---
+"@nailyjs/typeorm": patch
+---
+
+feat(typeorm): add global types
--- packages/typeorm/src/types.ts
+++ packages/typeorm/src/types.ts
@@ -1,8 +1,22 @@
-import type { EntitySchema, MixedList } from 'typeorm'
+import type { DataSourceOptions, EntitySchema, MixedList } from 'typeorm'
 
 // eslint-disable-next-line ts/no-unsafe-function-type
 export type Entities = MixedList<Function | string | EntitySchema>
 export type AsyncableCallback<T> = () => Promise<T> | T
 export interface TypeOrmPluginOptions {
   entities?: Entities | AsyncableCallback<Entities>
+}
+
+declare global {
+  namespace Naily {
+    namespace Configuration {
+      interface NailyUserConfig {
+        /** TypeORM configuration */
+        typeorm?: Omit<DataSourceOptions, 'entities'>
+      }
+      interface NailyUserIntelliSense {
+        typeorm?: DataSourceOptions
+      }
+    }
+  }
 }
