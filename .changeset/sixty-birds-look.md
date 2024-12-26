4e0932c6dfd9470ec65b3f0bbdf01c2cebbabff4
Naily <zero@naily.cc>
2024-12-26 23:46:53 +0800

    docs(changeset): feat: update rpc

---
+++ .changeset/sixty-birds-look.md
@@ -0,0 +1,5 @@
+---
+"@nailyjs/electron": patch
+---
+
+feat: update rpc
--- packages/electron/src/rpc.ts
+++ packages/electron/src/rpc.ts
@@ -1,26 +1,32 @@
 import type { InjectionToken } from '@nailyjs/ioc'
 import type { RpcServerRequest } from '@nailyjs/rpc'
-import { ipcRenderer } from 'electron'
 
 export interface ElectronRpcClientReturn {
   request<T extends Record<string, any>>(injectionToken: string | symbol): RpcServerRequest<T>
 }
 
-export function createElectronClient(): ElectronRpcClientReturn {
+export type InvokeFn = (channel: string, ...args: any[]) => Promise<any>
+
+export function createElectronClient(invokeFn: InvokeFn, handlerToken: string): ElectronRpcClientReturn {
   function request<T extends Record<string, any>>(injectionToken: InjectionToken): RpcServerRequest<T> {
     function createProxy(path: (string | symbol)[] = []): RpcServerRequest<T> {
       // eslint-disable-next-line ts/ban-ts-comment
       // @ts-expect-error
       return new Proxy(async () => {}, {
         async apply(_target, thisArg, argArray) {
-          const result = await ipcRenderer.invoke(injectionToken.toString(), {
+          let result = await invokeFn(handlerToken, {
             jsonrpc: '2.0',
             id: crypto.randomUUID(),
             method: `${injectionToken.toString()}.${path.join('.')}`,
             params: argArray,
           })
 
-          return result.data.result
+          result = JSON.parse(result)
+
+          if (result.error)
+            return result.error
+
+          return result.result
         },
 
         get(_, prop) {
